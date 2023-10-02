c76d4d6f6285ae5dd2faa5ff5b90a110ce1f91d7
February191999 <rye.t.hsu@gmail.com>
2023-10-02 17:34:18 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// window.onscroll = function() {
+//     myFunction()
+// };
+
+// var navbar = document.querySelector("navigation");
+// var sticky = navbar.offsetTop;
+
+// function myFunction() {
+//     if (window.scrollY >= sticky) {
+//         navbar.classList.add("sticky")
+//     } else {
+//         navbar.classList.remove("sticky");
+//     }
+// }
+
+// let number;
+
+// window.onscroll = function(){
+//     number = window.scrollY;
+//     if(number > 10){
+//         document.querySelector(".stick").style.background = '#fff';
+//     }
+//     else{
+//         document.querySelector(".stick").style.background = 'none';
+//     }
+// }
